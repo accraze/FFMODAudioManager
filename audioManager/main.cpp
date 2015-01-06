ce71d97cc66ba71b2c98202624af0fa70c8f2a1b
accraze <accraze@gmail.com>
2015-01-06 09:36:09 -0700

    Initial Commit

---
+++ audioManager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  audioManager
+//
+//  Created by Andrew Craze on 1/6/15.
+//  Copyright (c) 2015 Andrew Craze. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
