4ad148106859bf1f3f3f00202d730ca9268924a5
accraze <accraze@gmail.com>
2015-01-08 17:24:40 -0700

    testing load and play driver

--- main.cpp
+++ main.cpp
@@ -16,4 +16,28 @@ int main(int argc, const char * argv[]) {
     
     // initialize
     AudioManager audio;
+    audio.Load("explosion.wav");
+
+    //game loop
+    while (window.isOpen()) {
+      //60 frames per second
+      float elapsed = clock.getElapsedTime().asSeconds();
+      if(elapsed < 1.0f / 60.0f) continue;
+      clock.restart();
+      sf::Event event;
+      while (window.pollEvent (event)) {
+        //handle Window Events
+        if(event.type == sf::Event::Closed)
+          window.close();
+
+        //handle user input
+        if(event.type ==sf::Event::KeyPressed && 
+           event.key.coce == sf::Keyboard::Space)
+          audio.Play("explosion.wav");
+      }
+
+      audio.Update(elapsed);
+    }
+    
+    return 0;
 }
